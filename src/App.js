395a233268afac825a2fb2840e10610d182082bd
Vishnu Baliga <vishnubaliga@Baligas-MacBook-Pro.local>
2019-08-14 14:10:59 +0530

    project init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+      /* Here’s the juicy bit for posting the form submission */
+
+      handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", ...this.state })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+  
+        e.preventDefault();
+      };
+  
+      handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render(){
+    const { name, email, message } = this.state;
+    return(
+      
+      <div className="App">
+        <header className="App-header">  
+  
+  
+        <form onSubmit={this.handleSubmit}>
+            <p>
+              <label>
+                Please enter your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" value={message} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+  
+  
+        </header>
+      </div>
+
+    )
+  }
 }
 
 export default App;
