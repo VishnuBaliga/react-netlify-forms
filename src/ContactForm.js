5c9361a5875ef505f6ff60eb801226ab38869d07
Vishnu Baliga <vishnubaliga@Baligas-MacBook-Pro.local>
2019-08-14 15:10:26 +0530

    adding hidden field in forms

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -32,6 +32,7 @@ const encode = (data) => {
       const { name, email, message } = this.state;
       return (
         <form name="contact" netlify onSubmit={this.handleSubmit}>
+            <input type="hidden" name="form-name" value="the-name-of-the-html-form" />
           <p>
             <label>
               Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
